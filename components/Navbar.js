d1b49b160bff1412fd256fd21605190facae4c4e
Aniket Bharane <codeaniket123@gmail.com>
2024-06-23 17:59:44 +0530

    cart functionality

--- components/Navbar.js
+++ components/Navbar.js
@@ -5,7 +5,7 @@ import { FaOpencart, FaCirclePlus, FaCircleMinus } from "react-icons/fa6";
 import { IoCloseCircle, IoBagCheckSharp } from "react-icons/io5";
 import { CgTrashEmpty } from "react-icons/cg";
 
-function Navbar() {
+function Navbar({ cart, addtocart, removefromcart, clearCart, subtotal }) {
   const togglecart = () => {
     if (ref.current.classList.contains('translate-x-full')) {
       ref.current.classList.remove('translate-x-full')
@@ -20,8 +20,8 @@ function Navbar() {
   return (
     <div className='flex flex-col md:justify-start md:flex-row justify-center items-center py-2 mx-3 shadow-md'>
       <div>
-        <Link href={'/'} >
-          <Image src={'/logo.png'} width={200} height={40} />
+        <Link href={'/'}>
+          <Image src={'/logo.png'} width={200} height={40} alt='logo' />
         </Link>
       </div>
 
@@ -43,92 +43,27 @@ function Navbar() {
         <span className='absolute top-5 right-2 cursor-pointer text-xl text-pink-500' onClick={togglecart} ><IoCloseCircle /></span>
 
         <ol className='list-decimal'>
-          <li>
+
+          {Object.keys(cart).length == 0 && <div className='mt-5 text-center font-semibold' >Your cart is empty!! </div>}
+
+          {Object.keys(cart).map((k) => {return <li key={k}>
             <div className="items flex">
-              <div className='flex w-2/3 my-5' >T-shirt - wear the code</div>
+              <div className='flex w-2/3 my-5' >{cart[k].name}</div>
               <div className=' flex w-1/3 items-center justify-center' >
-                <FaCircleMinus className='text-pink-500' />
-                <span className='mx-2' >1</span>
-                <FaCirclePlus className='text-pink-500' />
+                <FaCircleMinus 
+                onClick={()=>{removefromcart(k, 1, 499, "Wear the code", "XL", "red")}} className='text-pink-500' />
+                <span className='mx-2' >{cart[k].qty}</span>
+                <FaCirclePlus onClick={()=>{addtocart(k, 1, 499, "Wear the code", "XL", "red")}} className='text-pink-500' />
               </div>
             </div>
           </li>
-          <li>
-            <div className="items flex">
-              <div className='flex w-2/3 my-5' >T-shirt - wear the code</div>
-              <div className=' flex w-1/3 items-center justify-center' >
-                <FaCircleMinus className='text-pink-500' />
-                <span className='mx-2' >1</span>
-                <FaCirclePlus className='text-pink-500' />
-              </div>
-            </div>
-          </li>
-          <li>
-            <div className="items flex">
-              <div className='flex w-2/3 my-5' >T-shirt - wear the code</div>
-              <div className=' flex w-1/3 items-center justify-center' >
-                <FaCircleMinus className='text-pink-500' />
-                <span className='mx-2' >1</span>
-                <FaCirclePlus className='text-pink-500' />
-              </div>
-            </div>
-          </li>
-          <li>
-            <div className="items flex">
-              <div className='flex w-2/3 my-5' >T-shirt - wear the code</div>
-              <div className=' flex w-1/3 items-center justify-center' >
-                <FaCircleMinus className='text-pink-500' />
-                <span className='mx-2' >1</span>
-                <FaCirclePlus className='text-pink-500' />
-              </div>
-            </div>
-          </li>
-          <li>
-            <div className="items flex">
-              <div className='flex w-2/3 my-5' >T-shirt - wear the code</div>
-              <div className=' flex w-1/3 items-center justify-center' >
-                <FaCircleMinus className='text-pink-500' />
-                <span className='mx-2' >1</span>
-                <FaCirclePlus className='text-pink-500' />
-              </div>
-            </div>
-          </li>
-          <li>
-            <div className="items flex">
-              <div className='flex w-2/3 my-5' >T-shirt - wear the code</div>
-              <div className=' flex w-1/3 items-center justify-center' >
-                <FaCircleMinus className='text-pink-500' />
-                <span className='mx-2' >1</span>
-                <FaCirclePlus className='text-pink-500' />
-              </div>
-            </div>
-          </li>
-          <li>
-            <div className="items flex">
-              <div className='flex w-2/3 my-5' >T-shirt - wear the code</div>
-              <div className=' flex w-1/3 items-center justify-center' >
-                <FaCircleMinus className='text-pink-500' />
-                <span className='mx-2' >1</span>
-                <FaCirclePlus className='text-pink-500' />
-              </div>
-            </div>
-          </li>
-          <li>
-            <div className="items flex">
-              <div className='flex w-2/3 my-5' >T-shirt - wear the code</div>
-              <div className=' flex w-1/3 items-center justify-center' >
-                <FaCircleMinus className='text-pink-500' />
-                <span className='mx-2' >1</span>
-                <FaCirclePlus className='text-pink-500' />
-              </div>
-            </div>
-          </li>
+          })}
         </ol>
         <div className="flex">
 
-          <button class="flex mx-2 mt-16 text-white bg-pink-500 border-0 py-2 px-3 focus:outline-none hover:bg-pink-600 rounded text-sm"> <IoBagCheckSharp className='m-0.5 ' />
+          <button className="flex mx-2 mt-16 text-white bg-pink-500 border-0 py-2 px-3 focus:outline-none hover:bg-pink-600 rounded text-sm"> <IoBagCheckSharp className='m-0.5 ' />
             Check Out</button>
-          <button class="flex mx-auto mt-16 text-white bg-pink-500 border-0 py-2 px-3 focus:outline-none hover:bg-pink-600 rounded text-sm"><CgTrashEmpty  className='m-0.5' />
+          <button onClick={clearCart} className="flex mx-auto mt-16 text-white bg-pink-500 border-0 py-2 px-3 focus:outline-none hover:bg-pink-600 rounded text-sm"><CgTrashEmpty className='m-0.5' />
             Clear cart</button>
         </div>
       </div>
--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,83 @@
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 import "@/styles/globals.css";
+import { useEffect, useState } from "react";
 
 export default function App({ Component, pageProps }) {
-  return <>
-  <Navbar/>
-  <Component {...pageProps} />;
-  <Footer/>
-  </>
+  const [cart, setCart] = useState({});
+  const [subtotal, setSubtotal] = useState(0);
+
+  useEffect(() => {
+    try {
+      if (localStorage.getItem("cart"))
+        setCart(JSON.parse(localStorage.getItem("cart")));
+    } catch (error) {
+      console.log(error);
+      localStorage.clear();
+    }
+  }, []);
+
+  const saveCart = (newcart) => {
+    // console.log(newcart); 
+    localStorage.setItem("cart",JSON.stringify(newcart));
+    let subt = 0;
+    let keys = Object.keys(newcart);
+
+    // console.log(keys);
+    for (let a = 0; a < keys.length; a++) {
+      subt += newcart[keys[a]].price * newcart[keys[a]].qty;
+    }
+
+    setSubtotal(subt);
+  };
+
+  const addtocart = (itemCode, qty, price, name, size, variant) => {
+    let newCart = cart;
+    if (itemCode in cart) {
+      newCart[itemCode].qty = cart[itemCode].qty + qty;
+    } else {
+      newCart[itemCode] = { qty: 1, price, name, size, variant };
+    }
+    setCart(newCart);
+    saveCart(newCart);
+  };
+
+  const removefromcart = (itemCode, qty, price, name, size, variant) => {
+    let newCart = cart;
+
+    if (itemCode in cart) {
+      newCart[itemCode].qty = cart[itemCode].qty - qty;
+    }
+    if (newCart[itemCode].qty <= 0) {
+      delete newCart[itemCode];
+    }
+    setCart(newCart);
+    saveCart(newCart);
+  };
+
+  const clearCart = () => {
+    setCart({});
+    saveCart({});
+  };
+  return (
+    <>
+      <Navbar
+        cart={cart}
+        addtocart={addtocart}
+        removefromcart={removefromcart}
+        clearCart={clearCart}
+        subtotal={subtotal}
+      />
+      <Component
+        cart={cart}
+        addtocart={addtocart}
+        removefromcart={removefromcart}
+        clearCart={clearCart}
+        subtotal={subtotal}
+        {...pageProps}
+      />
+      ;
+      <Footer />
+    </>
+  );
 }
